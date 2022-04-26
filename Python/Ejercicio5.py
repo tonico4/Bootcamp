b530303aa41405a60dd09044a083ab99885a63eb
Antonio García García <antogarcia.personal@gmail.com>
2022-04-26 22:22:39 +0200

    Python

---
+++ Python/Ejercicio5.py
@@ -0,0 +1,19 @@
+nombre = 'Toni'
+apellidos = 'García García'
+edad = 28
+email = 'tonigg4@gmail.com'
+telefono = 610277091
+casado = False
+hijos = False
+amigos = ['Álvaro', 'Vicente', 'Alex']
+peliculas = {'Acción':'Terminator', 'Comedia':'Hotel Transilvania', 'Terror':'Expediente Warren'}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(amigos)
+print(peliculas)
