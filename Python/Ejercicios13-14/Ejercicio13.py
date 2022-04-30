22f6bb8c93891ef9098689b1c8223dbbc9523b3b
Antonio García García <antogarcia.personal@gmail.com>
2022-04-30 19:32:38 +0200

    No lo entiendo

--- Python/Ejercicios13-14/Ejercicio13.py
+++ Python/Ejercicios13-14/Ejercicio13.py
@@ -1,11 +1,16 @@
 class Vehiculo:
-    color = 'red'
-    puertas = 5
-    ruedas = 4
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
 
 class Coche(Vehiculo):
-    velocidad = 0
-    cilindrada = 125
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-coche = Coche()
+coche = Coche('Red', 4, 5, 120, 125)
 print(coche)
