294a19d188a4707e9ed322bbcbfc70be0a0ada87
Antonio García García <antogarcia.personal@gmail.com>
2022-05-01 20:26:06 +0200

    Añadido ejercicio1 tema 8

--- Python/Tema7/Ejercicio2/Ejercicio2.py
+++ Python/Tema7/Ejercicio2/Ejercicio2.py
@@ -6,4 +6,4 @@ minutos = time.strftime('%M')
 if int(hora) >= 19:
     print('Deberías estar en casa')
 else:
-    print('Quedan {} horas y {} minutos para irte a casa'.format(18- int(hora), 59- int(minutos)))
+    print('Quedan', 18 - int(hora), 'horas', 59 - int(minutos), 'minutos para irte a casa')
---
+++ Python/Tema8/Ejercicio1.py
@@ -0,0 +1,6 @@
+fichero = open('fichero.txt', 'a')
+
+fichero.write('\n')
+fichero.write('Añado una línea nueva con a y ejecuto el programa')
+
+fichero.close()
