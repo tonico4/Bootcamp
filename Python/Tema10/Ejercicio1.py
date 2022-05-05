383973fb5b2c35cfafc61f2aa4579ed89be92601
Antonio García García <antogarcia.personal@gmail.com>
2022-05-05 19:48:03 +0200

    Ejercicio 2 tema 10 añadido

--- Python/Tema10/Ejercicio1.py
+++ Python/Tema10/Ejercicio1.py
@@ -12,9 +12,6 @@ root.title("RadioList")
 
 frame = ttk.Frame(root)
 frame.pack()
-#frame.grid(row=5)
-#root.columnconfigure(0, weight=1)
-#root.rowconfigure(0, weight=1)
 
 ## Radiobuttons
 seleccion = StringVar()
@@ -24,19 +21,13 @@ opcion2 = ttk.Radiobutton(root, text='Opcion 2', value=2, variable=seleccion, co
 opcion3 = ttk.Radiobutton(root, text='Opcion 3', value=3, variable=seleccion, command=mostrar)
 opcion4 = ttk.Radiobutton(root, text='Opcion 4', value=4, variable=seleccion, command=mostrar)
 
-#opcion1.grid(column=0, row=0, sticky='N')
-#opcion2.grid(column=0, row=1, sticky='N')
-#opcion3.grid(column=0, row=2, sticky='N')
-#opcion4.grid(column=0, row=3, sticky='N')
 opcion1.pack(fill='both', padx=100)
 opcion2.pack(fill='both', padx=100)
 opcion3.pack(fill='both', padx=100)
 opcion4.pack(fill='both', padx=100)
 
 ##Button
-
 reset = ttk.Button(root, text='RESET', command=resetear)
-#reset.grid(column=0, row=4, sticky='N')
 reset.pack()
 
 root.mainloop()
---
+++ Python/Tema10/Ejercicio2.py
@@ -0,0 +1,29 @@
+import tkinter
+from tkinter import END, Label, ttk
+from turtle import color
+
+class Aplicacion(ttk.Frame):
+    def __init__(self, ventana):
+        super().__init__(ventana)
+        ventana.title('Lista de países')
+
+        self.listbox = tkinter.Listbox(self, justify='center')
+        self.listbox.pack()
+
+        items = ("España", 
+                "Francia",
+                "Portugal",
+                "Italia",
+                "Alemania")
+        self.listbox.insert(0, *items)
+        print(self.listbox)
+
+        label = Label(text='Lista de países', color='red')
+        label.pack()
+
+        self.pack()
+        
+
+ventana = tkinter.Tk()
+app = Aplicacion(ventana)
+app.mainloop()
