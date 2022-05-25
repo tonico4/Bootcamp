9d2ceb53a4d106775a6ddbdd63610c115eb9ab62
Antonio García García <antogarcia.personal@gmail.com>
2022-05-25 09:34:30 +0200

    Ejercicio 1

---
+++ Java/Tema1/Ejercicio1.java
@@ -0,0 +1,24 @@
+class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        // Numbers
+        byte a = 2;
+        short b = 24;
+        int c = 131; // Most used
+        long d = 31123; // Most used
+
+        // Decimals
+        float e = 23.4f;
+        double f = 1321421.353d; // Most used
+
+        // Boolean
+        boolean g = true;
+        boolean h = false;
+
+        // Text
+        char i = 'g';
+        String j = "asfFAsfa"; // Most used
+    }
+
+}
