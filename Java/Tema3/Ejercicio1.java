b7547a3528d1e7354173fb33fc2192f4a3dd985c
Antonio García García <antogarcia.personal@gmail.com>
2022-06-01 19:07:55 +0200

    Tema 3

---
+++ Java/Tema3/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Java.Tema3;
+
+class Ejercicio1 {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Jesús", "Juan", "Mari", "Paula"};
+        
+        for(int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
