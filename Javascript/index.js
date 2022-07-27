c940fb7bf980f1c6c5d5aedd0af8bdd23898e054
Antonio García García <antogarcia.personal@gmail.com>
2022-07-27 09:59:16 +0200

    Ejercicio2

---
+++ Javascript/index.js
@@ -0,0 +1,12 @@
+const lista = [
+    28,
+    true,
+    new Date("april 30 1994"),
+    "Toni",
+    book = {
+        title: "The Hound of the Baskervilles",
+        author: "Arthur Conan Doyle",
+        date: new Date("november 15 2015"),
+        url: "https://www.amazon.es/Hound-Baskervilles-Wisehouse-Classics/dp/9176370666"
+    }
+];
