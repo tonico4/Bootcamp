c7f8fdddb54baeffa3b1b7ffa0791e4429af6c47
Antonio García García <antogarcia.personal@gmail.com>
2022-08-01 20:55:08 +0200

    Ejer4

---
+++ Javascript/Ejercicio04/index.js
@@ -0,0 +1,19 @@
+let nombre = "Toni";
+
+let apellido = "García";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let numLetras = estudiante.length;
+
+let firstCharName = nombre[0];
+
+let lastCharApe = apellido[5];
+
+let sinEspacios = estudiante.split(" ").join("");
+
+let isNombre = estudiante.includes(nombre);
