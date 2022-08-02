e639213f37a7ab42032f435de367414a5e8d2e36
Antonio García García <antogarcia.personal@gmail.com>
2022-08-02 19:32:45 +0200

    Ejercicio5

---
+++ Javascript/Ejercicio05/index.js
@@ -0,0 +1,13 @@
+let altura_cm = 174;
+
+let altura_m = 1.74;
+
+let peso_kg = 82.2;
+
+let altura = Math.ceil(altura_m);
+
+let peso = Math.round(peso_kg);
+
+let maxJS = Number.MAX_VALUE;
+
+let YesOrNo = Boolean((maxJS + 1) === maxJS)
