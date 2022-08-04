e1b107f461dccbe0a54a00107de7318307a73934
Antonio García García <antogarcia.personal@gmail.com>
2022-08-04 22:18:47 +0200

    funcionesJS

---
+++ Javascript/Ejercicio08/funciones.js
@@ -0,0 +1,19 @@
+function siempreTrue () {
+    return true
+}
+
+const asin = setTimeout((a => console.log("Hola soy una promesa")), 5000)
+
+function* genIdPar() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
+
+const gen = genIdPar()
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
