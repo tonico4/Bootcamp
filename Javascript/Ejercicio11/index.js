e310dfdc063a27232f28b53af4096045632d3426
Antonio García García <antogarcia.personal@gmail.com>
2022-08-13 10:54:46 +0200

    Ejercicio 11

---
+++ Javascript/Ejercicio11/index.js
@@ -0,0 +1,15 @@
+class Estudiante {
+    nombre = "Toni"
+    asignaturas = ["Javascript", "HTML", "CSS"]
+
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+}
+
+const estudiante = new Estudiante()
+
+console.log(estudiante.obtenDatos())
