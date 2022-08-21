88d7190cb3137edf54e447cf52ba27300ea79b07
tonico4 <antogarcia.personal@gmail.com>
2022-08-21 17:43:45 +0200

    Haciendo Ejercicio 16

---
+++ Javascript/Ejercicio16/ejercicio/index.js
@@ -0,0 +1,22 @@
+const parrafos = document.querySelectorAll("parrafo")
+const secciones = document.querySelectorAll("seccion")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", () => {
+
+    })
+
+    parrafo.addEventListener("dragend", () => {
+
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", () => {
+
+    })
+
+    seccion.addEventListener("drop", () => {
+        
+    })
+})
