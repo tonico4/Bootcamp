f003c91d2a8d7d0e533432b9f0bffa5db787f379
tonico4 <antogarcia.personal@gmail.com>
2022-08-22 13:32:25 +0200

    Ejercicio 16 acabado

--- Javascript/Ejercicio16/ejercicio/index.js
+++ Javascript/Ejercicio16/ejercicio/index.js
@@ -1,22 +1,35 @@
-const parrafos = document.querySelectorAll("parrafo")
-const secciones = document.querySelectorAll("seccion")
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+const basura = document.querySelector("#papelera")
 
 parrafos.forEach(parrafo => {
-    parrafo.addEventListener("dragstart", () => {
-
+    parrafo.addEventListener("dragstart", evento => {
+        parrafo.classList.add("dragging")
+        evento.dataTransfer.setData("id", parrafo.id)
     })
 
     parrafo.addEventListener("dragend", () => {
-
+        parrafo.classList.remove("dragging")
     })
 })
 
 secciones.forEach(seccion => {
-    seccion.addEventListener("dragover", () => {
-
+    seccion.addEventListener("dragover", evento => {
+        evento.preventDefault()
     })
 
-    seccion.addEventListener("drop", () => {
-        
+    seccion.addEventListener("drop", evento => {
+        const id_parrafo = evento.dataTransfer.getData("id")
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
     })
+})
+
+basura.addEventListener("dragover", evento => {
+    evento.preventDefault()
+})
+
+basura.addEventListener("drop", evento => {
+    const id_parrafo = evento.dataTransfer.getData("id")
+    document.getElementById(id_parrafo).remove()
 })
