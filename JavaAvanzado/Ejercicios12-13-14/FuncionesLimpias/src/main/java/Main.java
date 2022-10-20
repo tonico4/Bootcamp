765dd44a6a9111fac51fd5531160199a9e558bec
Antonio García <antogarcia.personal@gmail.com>
2022-10-20 17:33:39 +0200

    Ejercicio de funciones limpias acabado

---
+++ JavaAvanzado/Ejercicios12-13-14/FuncionesLimpias/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int sumarDosNumeros(int a, int b) {
+        return a + b;
+    }
+
+    public static void saludarNombrePropio(String name) {
+        System.out.println("Hola "+ name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumarDosNumeros(2, 3));
+        saludarNombrePropio("Jesús");
+    }
+}
