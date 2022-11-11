86db44d7582422fa62a992a1d0b28c2b1c1775ff
tonico4 <antogarcia.personal@gmail.com>
2022-11-11 12:34:48 +0100

    Acabando ejercicio 10

---
+++ Spring/Ejercicios10-11-12/ejercicios789/src/main/java/com/obspringtest/ejercicios789/entities/Laptop.java
@@ -0,0 +1,57 @@
+package com.obspringtest.ejercicios789.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "laptops")
+public class Laptop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private Integer yearbuild;
+
+    public Laptop() {
+    }
+
+    public Laptop(Long id, String brand, String model, Integer yearbuild) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.yearbuild = yearbuild;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYearbuild() {
+        return yearbuild;
+    }
+
+    public void setYearbuild(Integer yearbuild) {
+        this.yearbuild = yearbuild;
+    }
+}
---
+++ Spring/Ejercicios10-11-12/ejercicios789/src/test/java/com/obspringtest/ejercicios789/Ejercicios789ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obspringtest.ejercicios789;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicios789ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
