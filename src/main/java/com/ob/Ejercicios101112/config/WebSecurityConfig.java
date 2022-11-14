6e02a343031d7ade677bbc9b7b274108ea0480dc
tonico4 <antogarcia.personal@gmail.com>
2022-11-14 19:20:30 +0100

    Ejercicios 10, 11 y 12

---
+++ src/main/java/com/ob/Ejercicios101112/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ob.Ejercicios101112.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("Jose").password("1234").roles("USER").build();
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("Luis").password("4567").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
