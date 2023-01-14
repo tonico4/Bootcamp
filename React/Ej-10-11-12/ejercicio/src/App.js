f63426a14e3e2d28e04f57d95400fc24a58806b0
tonico4 <antogarcia.personal@gmail.com>
2023-01-14 20:55:00 +0100

    Changes

---
+++ React/Ej-10-11-12/ejercicio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SquareComponent from './components/SquareComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SquareComponent></SquareComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Ej-10-11-12/ejercicio/src/components/SquareComponent.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+
+const SquareComponent = () => {
+
+    const red = getRandomInt();
+    const green = getRandomInt();
+    const blue = getRandomInt();
+    
+    const intialStyles = {
+        backgroundColor: `rgb(0, 0, 0)`,
+        width: 255,
+        height: 255
+    }
+
+    const [styles, setStyles] = useState(intialStyles);
+
+    function getRandomInt() {
+        return Math.floor(Math.random() * 256);
+    }
+
+    function updateBackground() {
+        setStyles({
+            backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+            width: 255,
+            height: 255
+        })
+    }
+
+    function backStyles() {
+        setStyles(intialStyles);
+    }
+
+    
+
+    useEffect(() => {
+        const intervalId = setInterval(updateBackground(), 1000)
+        return () => {
+            
+        };
+    }, []);
+
+    return (
+        <div onMouseEnter={updateBackground} onMouseLeave={backStyles} onDoubleClick={backStyles} style={styles}>
+
+        </div>
+    );
+}
+
+export default SquareComponent;
